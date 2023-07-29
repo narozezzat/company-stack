328e78d1c6e9b7215b56acb6efe31d1a91185d25
Naroz Ezzat <narozezzat10@gmail.com>
2023-07-29 19:04:37 +0300

    edit in footer

--- componenet/Footer.js
+++ componenet/Footer.js
@@ -8,29 +8,61 @@ function Footer() {
   return (
     <>
       <div className="footer mt-5 container-fluid bg-light mt-5">
-        <div className="justify-comtent-center pt-3 row">
+        <div className="justify-content-center pt-3 row">
 
-          <div className="footer-logo col-md-3 col-sm-4 mx-3">
+          <div className="col-md-3 col-sm-12 justify-content-center">
             <Link href="#">
               <Image title="Groove Street, Home" alt="Logo" src={logo}  style={{height: "90px", width: "70px"}}/>
             </Link>
-          </div>
 
-          <div className="flex-row mt-1 mb-2 justify-content-start list-group mx-3">
+            <div className="flex-row mt-1 mb-2 justify-content-start list-group">
 
-            <div className="social-media-item border-0 bg-transparent list-group-item">
-              <Link href="https://www.twitter.com/stackinfoio" target="_blank" rel="noreferrer">
-                <TwitterOutlined />
-              </Link>
+              <div className="social-media-item border-0 bg-transparent list-group-item">
+                <Link href="https://www.twitter.com/stackinfoio" target="_blank" rel="noreferrer">
+                  <TwitterOutlined />
+                </Link>
+              </div>
+
+              <div className="social-media-item border-0 bg-transparent list-group-item">
+                <Link href="https://www.linkedin.com/company/stackinfoio" target="_blank" rel="noreferrer">
+                  <LinkedinOutlined />
+                </Link>
+              </div>
+
             </div>
 
-            <div className="social-media-item border-0 bg-transparent list-group-item">
-              <Link href="https://www.linkedin.com/company/stackinfoio" target="_blank" rel="noreferrer">
-                <LinkedinOutlined />
-              </Link>
+          </div>
+
+          <div className="col-md-3 col-sm-12">
+            <h3>Discover</h3>
+            <div className="mx-4">
+              <Link href="/" className="d-block my-4">Home</Link>
+              <Link href="#">Search</Link>
             </div>
 
           </div>
+
+          <div className="col-md-3 col-sm-12">
+            <h3>Who we are</h3>
+            <div className="mx-4">
+              <Link href="/" className="d-block my-4">About</Link>
+              <Link href="#">Privecy</Link>
+            </div>
+
+          </div>
+
+          <div className="col-md-3 col-sm-12">
+            <h3>Stack Info</h3>
+            <div className="my-4">
+              <p>Stack Info allows you to explore and find tech
+                 stacks used by more than 250 startups and corporates in EMEA 🌠💻
+              </p>
+            </div>
+
+          </div>
+
+
+          
 
           
 
