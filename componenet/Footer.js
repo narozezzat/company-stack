e53a2a89f4c3c5de029973614dd534beaff2e261
Naroz Ezzat <narozezzat10@gmail.com>
2023-07-29 19:48:40 +0300

    edit in design in footer

--- componenet/Footer.js
+++ componenet/Footer.js
@@ -10,62 +10,57 @@ function Footer() {
       <div className="footer mt-5 container-fluid bg-light mt-5">
         <div className="justify-content-center pt-3 row">
 
-          <div className="col-md-3 col-sm-12 justify-content-center">
+          <div className="col-md-3 col-sm-12">
             <Link href="#">
-              <Image title="Groove Street, Home" alt="Logo" src={logo}  style={{height: "90px", width: "70px"}}/>
+              <Image className="mx-3" title="Groove Street, Home" alt="Logo" src={logo}  style={{height: "90px", width: "90px"}}/>
             </Link>
 
-            <div className="flex-row mt-1 mb-2 justify-content-start list-group">
+            <div className="flex-row mb-2 justify-content-start list-group">
 
-              <div className="social-media-item border-0 bg-transparent list-group-item">
+              <div className="social-media-item rounded-circle list-group-item">
                 <Link href="https://www.twitter.com/stackinfoio" target="_blank" rel="noreferrer">
                   <TwitterOutlined />
                 </Link>
               </div>
 
-              <div className="social-media-item border-0 bg-transparent list-group-item">
+              <div className="social-media-item rounded-circle list-group-item mx-2">
                 <Link href="https://www.linkedin.com/company/stackinfoio" target="_blank" rel="noreferrer">
                   <LinkedinOutlined />
                 </Link>
               </div>
-
             </div>
-
           </div>
 
           <div className="col-md-3 col-sm-12">
             <h3>Discover</h3>
             <div className="mx-4">
-              <Link href="/" className="d-block my-4">Home</Link>
-              <Link href="#">Search</Link>
+              <Link href="/" className="d-block mb-3">Home</Link>
+              <Link href="#" className="d-block mb-4">Search</Link>
             </div>
-
           </div>
 
           <div className="col-md-3 col-sm-12">
             <h3>Who we are</h3>
             <div className="mx-4">
-              <Link href="/" className="d-block my-4">About</Link>
-              <Link href="#">Privecy</Link>
+              <Link href="/" className="d-block mb-3">About</Link>
+              <Link href="#" className="d-block mb-3">Privecy</Link>
             </div>
-
           </div>
 
           <div className="col-md-3 col-sm-12">
             <h3>Stack Info</h3>
-            <div className="my-4">
+            <div>
               <p>Stack Info allows you to explore and find tech
                  stacks used by more than 250 startups and corporates in EMEA 🌠💻
               </p>
             </div>
-
           </div>
 
 
         </div>
         <hr />
-        <div class="justify-content-center row">
-          <div class="text-center col-sm-12">
+        <div className="justify-content-center row">
+          <div className="text-center col-sm-12">
             <p>Copyright Stack Company © 2023</p>
           </div>
         </div>
